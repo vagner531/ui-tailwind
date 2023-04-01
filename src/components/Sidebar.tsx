ee04578a640408a4f876f0050700cbd86aa71d2b
vagner santos <vagner131417@gmail.com>
2023-04-01 17:34:01 -0300

    feat: add logo sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,9 @@
+import { Logo } from "./icons/Logo";
+
 export function Sidebar() {
   return(
-    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 px-6 py-6"></aside>
+    <aside className="border-r border-white/10 w-80 fixed left-0 top-0 bottom-0 px-6 py-6">
+      <Logo className="text-white h-4 w-auto"/>
+    </aside>
   )
 }
